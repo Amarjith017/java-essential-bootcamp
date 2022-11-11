8dbdb29d4f11f5ea0833a569b9c8529847763826
Amarjith017 <107428713+Amarjith017@users.noreply.github.com>
2022-11-11 21:55:22 +0530

    AssignmentOne.java
    Assignment One(Day 3)

---
+++ AssignmentOne.java
@@ -0,0 +1,13 @@
+class AssignmentOne{
+  public static void main(String[] args) {
+   int a=10,b=2,sum,diff,prod,quot;
+    sum=a+b;
+    diff=a-b;
+    prod=a*b;
+    quot=a/b;
+    System.out.println("sum is "+ sum);
+    System.out.println("Difference is "+diff);
+    System.out.println("Product is "+prod);
+    System.out.println("Quotient is "+quot);
+  }
+}
